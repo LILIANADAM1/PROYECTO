cd6f3e5e225e5f03b5e83a5566aca4697027eed7
LILIANADAM1 <MMORENO.LILIANA@GMAIL.COM>
2024-05-14 12:24:58 +0200

    fd

--- questionModel.js
+++ questionModel.js
@@ -1,11 +1,13 @@
-// questionModel.js
-
 const mongoose = require('mongoose');
 
-// Definir el esquema de la pregunta y respuesta
+// Definir el esquema de la pregunta y respuestas
 const questionSchema = new mongoose.Schema({
     pregunta: { type: String, required: true },
-    respuesta: { type: String, required: true }
+    respuestas: [
+        { type: String, required: true },
+        { type: String, required: false },
+        { type: String, required: false }
+    ]
 });
 
 // Crear un modelo de pregunta basado en el esquema definido
